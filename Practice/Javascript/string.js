60d2a386356ee98619dc1de8f06b449f3fd86e96
GoofyExploit <groverlucky406@gmail.com>
2024-08-19 23:28:01 +0530

    Business name generator

---
+++ Practice/Javascript/string.js
@@ -0,0 +1,19 @@
+/*let a = 'luc\rky'
+console.log(a)
+*/
+/*
+let name1 = `Lucky`
+let name2 = `Pihu`
+console.log(`His name is ${name1} and his sister's name is ${name2}`)*/
+
+// console.log("har\"".length)
+let a = `Lucky`
+console.log(a.startsWith("Lu"))
+console.log(a.endsWith("ky"))
+let b = `PIHU`
+console.log(b.toLowerCase())
+let amount = `Please give Rs 1000`
+console.log(amount.indexOf(1000))
+console.log(amount.slice(15))
+let c = `lucky`
+console.log(c.replace("y","i"))
